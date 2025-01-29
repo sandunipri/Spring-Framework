5a270e150937830d8fe787a9f0896f050c9fceb6
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-01-29 11:34:06 +0530

    Configuration

---
+++ 05_Configuration/src/main/java/org/example/bean/SpringBean01.java
@@ -0,0 +1,10 @@
+package org.example.bean;
+
+import org.springframework.stereotype.Component;
+
+
+public class SpringBean01 {
+    public SpringBean01(){
+        System.out.println("SpringBean one Constructor");
+    }
+}
---
+++ 05_Configuration/src/main/java/org/example/bean/SpringBean02.java
@@ -0,0 +1,7 @@
+package org.example.bean;
+
+public class SpringBean02 {
+    public SpringBean02(){
+        System.out.println("SpringBean two Constructor");
+    }
+}

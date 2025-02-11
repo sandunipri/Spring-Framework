8232d412f6ae824e8e33a716d609255e5eb9fa85
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-02-11 14:44:50 +0530

    Spring boot

---
+++ A13_Spring_Boot/src/main/java/org/example/a13_spring_boot/A13SpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.example.a13_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A13SpringBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(A13SpringBootApplication.class, args);
+    }
+
+}
---
+++ A13_Spring_Boot/src/test/java/org/example/a13_spring_boot/A13SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.a13_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A13SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

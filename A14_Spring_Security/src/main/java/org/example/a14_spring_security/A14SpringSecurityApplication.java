f114dcfb13fa8f4a48617cc69d8681cc0aefa625
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-02-26 11:28:38 +0530

    spring security

---
+++ A14_Spring_Security/src/main/java/org/example/a14_spring_security/A14SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.a14_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A14SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A14SpringSecurityApplication.class, args);
+    }
+
+}
---
+++ A14_Spring_Security/src/test/java/org/example/a14_spring_security/A14SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.a14_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A14SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

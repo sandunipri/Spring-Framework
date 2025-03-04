0ea32c934a47f3eb6d2395dcdf26691c8776563e
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-03-04 21:11:47 +0530

    spring transaction

--- A13_Spring_Boot/src/main/resources/js/placeOrder.js
+++ A13_Spring_Boot/src/main/resources/js/placeOrder.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
 
     let Allcustomers = [];
     let AllItems = [];
+    let cartDetails = [];
+
+    let selectedItem = null;
+    let selectedCustomer = null;
 
     function getAllCustomers() {
         $.ajax({
@@ -51,6 +55,78 @@ $(document).ready(function () {
             }
         })
     }
+
+    $('#CustomerIdSelect').change(function () {
+        selectedCustomer = Allcustomers.find((customer) => {
+            return customer.id === $('#CustomerIdSelect').val()
+        })
+    })
+
+    $('#ItemIdSelected').change(function () {
+       selectedItem = AllItems.find((item) => {
+            return item.id === $('#ItemIdSelected').val()
+        })
+
+    })
+
+    $('#AddOrder').click(function () {
+        let qty = $('#qty').val();
+
+        if (selectedItem ==null || selectedCustomer == null) {
+            alert("Please select customer and item")
+            return;
+        }
+
+        let total = selectedItem.price * qty;
+
+        $('#orderTableBody').append(
+        /*    <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">CUSTOMER ID</th>
+                <th scope="col">ITEM CODE</th>
+                <th scope="col">QTY</th>
+                <th scope="col">PRICE</th>
+                <th scope="col">Actions</th>
+            </tr>
+            </thead>*/
+
+            `<tr>
+                <td>${selectedCustomer.id}</td>
+                <td>${selectedItem.code}</td>
+                <td>${qty}</td>
+                <td>${selectedItem.price}</td>
+                <td>${total}</td>
+                <td><button class="btn btn-danger">Delete</button></td>
+            </tr>`
+        )
+
+        selectedItem.qty = qty;
+        cartDetails.push(selectedItem)
+
+    });
+
+    $('#placeOder').click(function (){
+        let customerId  = $('#CustomerIdSelect').val();
+        let customer = Allcustomers.find((customer) => customer.id === Number(customerId))
+
+        $.ajax({
+            url :"",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                "customerDTO": customer,
+                "orderDetailsDTO": cartDetails
+            }),
+            success: (res) => {
+                alert("Order placed successfully")
+            },
+            error: (err) => {
+                alert("Something went wrong")
+                console.log(err)
+            }
+        })
+    })
 
     getAllCustomers();
     getAllItems();
